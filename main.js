5b1eb4a33b05bec4c92d4188af05c9d934f75e1f
Andy-W <andy.bartkiv@gmail.com>
2021-05-07 23:25:57 +0300

    ToDo: Project images, description, card animation

--- main.js
+++ main.js
@@ -1,10 +1,7 @@
 const panels = [...document.querySelectorAll('.panel')];
-// console.log(panels);
-
 panels.forEach(el => 
     el.addEventListener('click', () => {
-        let num = el.id.slice(-1); 
-        // console.log(panels[num]);
+        const num = el.id.slice(-1); 
         panels.forEach((panel, i) => {
             if (i !== num) panel.classList.remove('main');
         });
