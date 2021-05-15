33dbbb9298419a1de7578a77e207cadfc9d2e49f
Andy-W <andy.bartkiv@gmail.com>
2021-05-15 23:44:16 +0300

    finish new-slider

--- main.js
+++ main.js
@@ -4,9 +4,44 @@ panels.forEach(el => el.addEventListener('click', (event) => {
         event.currentTarget.classList.add('main');
     })
 ); 
+
 const projects = [...document.querySelectorAll('.project')];
-projects.forEach(el => el.addEventListener('click', (event) => {
-        projects.forEach(project => project.classList.remove('active'));
-        event.currentTarget.classList.add('active');
-    })
-);
+const delayContextHide = 450; //450
+
+let mql = window.matchMedia('(max-width: 90vh)');
+if (mql.matches) projects.reverse();
+mql.addEventListener("change", reverseTabs);
+
+let activeTab = 0;
+toggleClassTrans(activeTab);
+projects[activeTab].classList.add('show');
+
+projects.forEach(el => el.addEventListener('click', openTab));
+
+
+// --------------------- FUNCTIONS ----------------------------------
+function toggleClassTrans(tab) {
+    projects.slice(0, tab).forEach(el => el.classList.remove('trans'));
+    projects.slice(tab).forEach(el => el.classList.add('trans'));
+};
+
+function toggleClassShow(tab) {
+    const tabClosed = projects.find(el => el.classList.contains('show'));
+    setTimeout(() => tabClosed.classList.remove('show'), delayContextHide);
+    projects[tab].classList.add('show');
+};
+
+function openTab(event) {
+    const tab = projects.indexOf(event.currentTarget);
+    if (activeTab !== tab) {
+        activeTab = tab;
+        toggleClassTrans(activeTab);
+        toggleClassShow(activeTab);
+    }
+};
+
+function reverseTabs() {
+    activeTab = projects.length - 1 - activeTab;
+    projects.reverse();
+    toggleClassTrans(activeTab);
+};
